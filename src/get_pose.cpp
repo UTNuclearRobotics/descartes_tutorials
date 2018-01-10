5db31a1eac1fd8e41526d968554268133b8952c2
Adam Allevato <kukanani@noreply.users.github.com>
2018-01-10 16:33:44 -0600

    Cleaned up printouts and fixed warning from bad move_interface include

--- src/get_pose.cpp
+++ src/get_pose.cpp
@@ -28,23 +28,16 @@ dataCollection::dataCollection() :
 {
     tf::TransformListener tfListener;
     moveInterface_.initialize("sia5");
-    moveInterface_.setPlannerId("RRTConnectkConfigDefault");
-    moveInterface_.setPlanningTime(10.0);
-
    
     geometry_msgs::PoseStamped pose;
     pose = moveInterface_.getCurrentPose();
     tfListener.waitForTransform(pose.header.frame_id.c_str(), frame_, ros::Time::now(), ros::Duration(10.0));
     tfListener.transformPose(frame_, pose, pose);
-/*    
-ROS_INFO_STREAM(pose); 
-*/    
-   
-    
+
+    ROS_INFO_STREAM("pose of frame " << moveInterface_.getMoveGroupInterface()->getEndEffectorLink() << " in frame " << frame_ << ": ");
+    ROS_INFO_STREAM("" << pose);
 
     std::vector<double> joints;
     joints = moveInterface_.getCurrentJointValues();
     ROS_INFO_STREAM("Joint_0=" << joints[0] << "Joint_1=" << joints[1] << "Joint_2=" << joints[2] << "Joint_3=" << joints[3] << "Joint_4=" << joints[4]<< "Joint_5=" << joints[5]<< "Joint_6=" << joints[6]);
-
- 
 }
